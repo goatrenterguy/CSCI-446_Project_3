0703edd04467d91a4d1618080933303ca0194bee
addariasfr <addariasfr@gmail.com>
2021-11-07 18:40:05 -0700

    better varaiable combination

--- ApproximateInference.py
+++ ApproximateInference.py
@@ -8,14 +8,14 @@ from BayesianNetwork import BayesianNetwork
 class ApproximateInference:
     def gibbsAsk(self, X, e, bnet: BayesianNetwork, n):
         # --- Create first sample ---
-        sample_list = []
-        sample = copy.deepcopy(e)
-        frontier = []
+        sample_list = []  # list of samples
+        sample = copy.deepcopy(e)  # first sample, initialized with evidence
+        frontier = []  # frontier nodes to explore
 
         # add root nodes to frontier
         for node in bnet.network.values():
             if len(node.parents) == 0:
-                frontier.append(node)  # add the nodes with no parents to the current network level
+                frontier.append(node)  # add the nodes with no parents to the frontier
 
         # sample root nodes
         root_nodes = copy.copy(frontier)
@@ -27,8 +27,8 @@ class ApproximateInference:
             for node in frontier:  # for each of the frontier nodes
                 if e.get(node.name) is None:  # if the node is not in the evidence
                     s_flag = True  # set the sample flag true by default
-                    for p in node.parents:  # check each parent of the node
-                        if p not in sample:  # if the parent has not been sampled yet
+                    for parent in node.parents:  # check each parent of the node
+                        if parent not in sample:  # if the parent has not been sampled yet
                             s_flag = False  # set the sample flag for the current node to false
                     if s_flag:  # if all parents have been sampled
                         sample, frontier = self.sample_node(node, bnet, sample, sample, frontier)  # sample the node
@@ -54,8 +54,8 @@ class ApproximateInference:
                 for node in frontier:  # for each of the frontier nodes
                     if e.get(node.name) is None:  # if the node is not in the evidence
                         s_flag = True  # set the sample flag true by default
-                        for p in node.parents:  # check each parent of the node
-                            if p not in sample:  # if the parent has not been sampled yet
+                        for parent in node.parents:  # check each parent of the node
+                            if parent not in sample:  # if the parent has not been sampled yet
                                 s_flag = False  # set the sample flag for the current node to false
                         if s_flag:  # sample the node according to the sample flag
                             sample, frontier = self.sample_node(node, bnet, sample, sample_list[i], frontier)  # sample
@@ -64,58 +64,45 @@ class ApproximateInference:
             sample_list.append(sample)
 
         # --- Combine sample values ---
-        probabilities = []
-        for variable in X:
-            states = []
-            for state in bnet.network[variable].states:
-                states.append(0)
+        results = []
+        for var in X:  # for each of the query variables
+            values = []  # set of observed values
             for sample in sample_list:
-                if sample[variable] == 'TRUE':
-                    states[0] += 1
-                elif sample[variable] == 'FALSE':
-                    states[1] += 1
-                elif sample[variable] == 'ZERO':
-                    states[0] += 1
-                elif sample[variable] == 'LOW':
-                    if len(states) == 4:
-                        states[1] += 1
-                    else:
-                        states[0] += 1
-                elif sample[variable] == 'NORMAL':
-                    if len(states) == 4:
-                        states[2] += 1
-                    elif variable == 'INTUBATION':
-                        states[0] += 1
-                    else:
-                        states[1] += 1
-                elif sample[variable] == 'HIGH':
-                    if len(states) == 4:
-                        states[3] += 1
-                    else:
-                        states[2] += 1
-            for i in range(len(states)):
-                states[i] /= n
-            probabilities.append(states)
-        return probabilities
+                values.append(sample[var])  # get the values for the query variable from each sample
+            dist = []  # probability distribution
+            for state in bnet.network[var].states:  # for each of the possible states of the query variable
+                count = 0  # initialize the value count
+                for val in values:  # for each value gathered from the samples
+                    if val == state:  # if the value matches the current state being counted
+                        count += 1  # count the times each state is observed
+                dist.append(str(state) + ": " + str(count / n))  # add the probability of observing the state to dist
+            results.append(str(var) + ": " + str(dist))  # add the variable's distribution to the result set
+        return results
 
     def sample_node(self, node: BayesianNode, bnet: BayesianNetwork, cur_sample, prev_sample, frontier):
         if len(node.parents) == 0:  # if the node has no parents
-            s_val = random.choice(node.states, 1, p=node.probabilities["table"])
+            s_val = random.choice(node.states, 1, p=node.probabilities["table"])  # get the probabilities
         else:
             p_vals = ()  # parent node values
-            for p in node.parents:  # for each parent of the current node
-                p_vals += (prev_sample.get(p),)  # add parent value from sample
-            denom = sum(float(prob) for prob in node.probabilities[p_vals])  # get denominator to normalize distribution
-            p = ()
-            for prob in node.probabilities[p_vals]:
-                p += (float(prob) / denom,)
+            for parent in node.parents:  # for each parent of the current node
+                p_vals += (prev_sample.get(parent),)  # add parent value from sample
+            p = self.normalize_distribution(node, p_vals)  # normalize distribution
             s_val = random.choice(node.states, 1, p=p)  # choose a state based on probabilities
         cur_sample[node.name] = s_val[0]  # add the sampled value to the set
         for child in node.children:  # for each child of the sampled node
             if bnet.network[child] not in frontier:
                 frontier.append(bnet.network[child])  # add the child to the frontier
         frontier.remove(node)  # remove the sampled node from the frontier
-        for node in frontier:
-            if node.name in cur_sample:
-                frontier.remove(node)
-        return cur_sample, frontier
+        for node in frontier:  # for each node in the frontier
+            if node.name in cur_sample:  # if the node has been sampled already
+                frontier.remove(node)  # remove it from the frontier
+        return cur_sample, frontier  # return the updated sample and frontier
+
+    # Utility method for normalizing probability distributions that don't add to 1 (i.e. [0.33, 0.33, 0.33])
+    @staticmethod
+    def normalize_distribution(node, p_vals):
+        denom = sum(float(prob) for prob in node.probabilities[p_vals])  # get denominator to normalize distribution
+        p = ()  # normalized distribution
+        for prob in node.probabilities[p_vals]:  # for each probability
+            p += (float(prob) / denom,)  # add the normalized probability to the distribution
+        return p
--- Main.py
+++ Main.py
@@ -15,16 +15,26 @@ class Main:
         }
 
     def selectionPrompt(self):
-        print("Select a network:\nA: Alarm\nC: Child\nH: Hailfinder\nI: Insurance\nW: Win95pts")
-        selection = input().upper()
+        #print("Select a network:\nA: Alarm\nC: Child\nH: Hailfinder\nI: Insurance\nW: Win95pts")
+        #selection = input().upper()
         # try:
-        filePath = "Networks/" + self.selectionKey[selection].lower() + ".bif"
-        BNet = self.fileParser.readFile(filePath)
-        print("File: " + filePath + " opened")
+        #filePath = "Networks/" + self.selectionKey[selection].lower() + ".bif"
+        #BNet = self.fileParser.readFile(filePath)
+        #print("File: " + filePath + " opened")
         # factor = ExactInference().eliminationAsk(X='HYPOVOLEMIA', e={"HRBP": "HIGH", "CO": "LOW", "BP": "HIGH"}, BNet=BNet)
-        sample_list = ApproximateInference().gibbsAsk(X=['HYPOVOLEMIA', 'LVFAILURE', 'ERRLOWOUTPUT'], e={"HRBP": "HIGH", "CO": "LOW", "BP": "HIGH"}, bnet=BNet, n=100)
-        print(len(sample_list))
-        print(sample_list)
+        BNet = self.fileParser.readFile('Networks/alarm.bif')
+        alarm_le = ApproximateInference().gibbsAsk(X=['HYPOVOLEMIA', 'LVFAILURE', 'ERRLOWOUTPUT'],
+                                                   e={"HRBP": "HIGH", "CO": "LOW", "BP": "HIGH"}, bnet=BNet, n=1000)
+        print('Alarm little evidence: ' + str(alarm_le))
+        alarm_me = ApproximateInference().gibbsAsk(X=['HYPOVOLEMIA', 'LVFAILURE', 'ERRLOWOUTPUT'],
+                                                   e={"HRBP": "HIGH", "CO": "LOW", "BP": "HIGH", 'HRSAT': 'LOW',
+                                                      'HREKG': 'LOW'}, bnet=BNet, n=1000)
+
+        print('Alarm moderate evidence: ' + str(alarm_me))
+
+        BNet = self.fileParser.readFile('Networks/child.bif')
+        child = ApproximateInference().gibbsAsk(X=['Disease'], e={'LowerBodyO2': '"<5"', 'RUQO2': '">=12"', 'CO2Report': '">=7.5"', 'XrayReport': 'Asy/Patchy'}, bnet=BNet, n=1000)
+        print('Child little evidence: ' + child)
         #print(factor.variables)
         #print(factor.cpt)
 
